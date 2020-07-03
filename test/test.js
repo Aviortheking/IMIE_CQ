0d7c37a03dafa1594797b04ce0bdc17d92995f9e
Neeko <brossard.nicolas09@gmail.com>
2020-07-03 10:32:03 +0200

    Ajout du test getByID => A toi de jouer flouflou

--- test/test.js
+++ test/test.js
@@ -26,12 +26,42 @@ describe("Test de l'index.js", () => {
     })
 })
 describe('APIController', () => {
-    it(" Doit retourner le resultat de l'appel à la méthode ?all de l'API, possible que ce soit un objet vide ", () => {
+    describe("Test des methode de l'API CONTROLLER", () => {
+        it(" Doit retourner le resultat de l'appel à la méthode ?all de l'API, possible que ce soit un objet vide ", () => {
+            //Arrange
+            const data = []
+            const req = {}
+            const db = {
+                getAll: () => {
+                    return data
+                },
+            }
+            const apiController = new ApiController(db)
+
+            let result = null
+            const res = {
+                send: (value) => {
+                    result = value
+                },
+            }
+
+            // Act
+            apiController.all(req, res)
+
+            // Assert
+            assert.equal(result, data)
+        })
+    })
+    it("Doit retouner le resultat de l'appel getByID par la route ?id=", () => {
         //Arrange
         const data = []
-        const req = {}
+        const req = {
+            query: {
+                id: '1',
+            },
+        }
         const db = {
-            getAll: () => {
+            getByID: () => {
                 return data
             },
         }
@@ -39,13 +69,13 @@ describe('APIController', () => {
 
         let result = null
         const res = {
-            send: (value) => {
+            getByID: (value) => {
                 result = value
             },
         }
 
         // Act
-        apiController.all(req, res)
+        apiController.getByID(req, res)
 
         // Assert
         assert.equal(result, data)
